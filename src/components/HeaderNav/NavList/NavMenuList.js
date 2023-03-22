0190417d3b3014812b410314e8354ea555970169
Bartosz Przymencki <bartoszprzymencki@gmail.com>
2023-03-22 15:56:57 +0100

    menu is did

--- src/components/HeaderNav/NavList/NavMenuList.js
+++ src/components/HeaderNav/NavList/NavMenuList.js
@@ -1,12 +1,20 @@
 import classes from "./NavMenuList.module.css";
 
+import { AiOutlineUser } from "react-icons/ai";
 import NavMenuItem from "./NavMenuItem";
 
 const NavMenuList = (props) => {
   const NavMenuItems = props.elements.map((item) => (
     <NavMenuItem key={item.id}>{item.type}</NavMenuItem>
   ));
-  return <ul className={classes.ul}>{NavMenuItems}</ul>;
+  return (
+    <ul className={classes.ul}>
+      {NavMenuItems}
+      <NavMenuItem>
+        Profil <AiOutlineUser />
+      </NavMenuItem>
+    </ul>
+  );
 };
 
 export default NavMenuList;
